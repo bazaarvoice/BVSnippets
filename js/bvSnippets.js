7f5a7818d41ac78cf58b20df96db609660de2051
bdb-bv <ben.balentine@bazaarvoice.com>
2013-05-15 12:46:42 -0500

    first functional proof

--- js/bvSnippets.js
+++ js/bvSnippets.js
@@ -1,11 +1,11 @@
-;(function($){
+(function($){
 /*******************************************************************************************/    
 // jquery.bvSnippets.js - version 0.1
 // A jQuery plugin for injecting common Bazaarvoice Data API snippets
 // 
 // Copyright (c) 2013, Liam Wiles, Ben Balentine
 // Bazaarvoice PRREP Project
-// https://github.com/bazaarvoice/HostedUIResources
+// https://github.com/bazaarvoice/BVSnippets
 // Created: 2013-05-10
 //
 // This example shows the javascript syntax
@@ -26,68 +26,69 @@
 // implied, including without limitation any implied warranties of condition,
 // uninterrupted use, merchantability, or fitness for a particular purpose.
 /*******************************************************************************************/
-	
-	// Begin compatability tests
-	/*  define foreach for older browsers */
-	if (!Array.prototype.forEach) {
-		Array.prototype.forEach = function (fn, scope) {
-			for (var i = 0, len = this.length; i < len; ++i) {
-				fn.call(scope || this, this[i], i, this);
-			}
-		}
-	}
-	// End compatability tests
+
 
 	$.fn.featuredReviews = function(bvhost, displaycode, apikey, options){
+		
 		var reviewContent = {}; //used to capture inline rating object callback
 		var featuredReviewString = ''; //used to concatenate products for batch query
 		var featuredReviewList = {}; //list of products to query
 
-		featuredReviewList = $(this).map(function(){return $(this).attr("data-id");}).get();
-		featuredReviewList.forEach(function(element,index,array){
+		this.each(function(element, index, array){
+			var element = $( this ).attr("data-id");
 			featuredReviewString+='&resource.'+element+'=reviews&filter.'+element+'=IsFeatured:true&Limit.'+element+'=1&Filter.'+element+'=ProductId:'+element+'&Sort.'+element+'=SubmissionTime:desc';
-		});
-		featuredReviews(featuredReviewList,function(){
-			featuredReviewList.forEach(function(element,index,array){
-				if(typeof reviewContent[element+'_location'] === 'string' ) {
-					$('#bvReviewProduct_'+element).html(reviewContent[element+'_location']);
-					$('#bvReviewProduct_'+element).append(reviewContent[element+'_name']);
-					$('#bvReview_'+element).html(reviewContent[element+'_rating']);
-					$('#bvReview_'+element).append(reviewContent[element+'_author']);
-					$('#bvReview_'+element).append(reviewContent[element+'_text'].substr(0,200));
-					$('#bvReview_'+element).append(reviewContent[element+'_link']);
+			featuredReviewList[index] = function(){
+				if(typeof reviewContent[element+'_name'] === 'string') {
+					console.log(reviewContent[element+'_name']+"bvElement_"+element);
+					$("#bvElement_"+element).html(reviewContent[element+'_name']);
+					$("#bvElement_"+element).append(reviewContent[element+'_rating']);
+					$("#bvElement_"+element).append(reviewContent[element+'_author']);
+					$("#bvElement_"+element).append(reviewContent[element+'_text'].substr(0,200));
+					$("#bvElement_"+element).append(reviewContent[element+'_link']);
 				}
+				else {
+					console.log(reviewContent);
+				}
+			};
+		});
+
+		pullReviews(function(){
+			$.each(featuredReviewList, function(){
+				this();			
 			});
 		});
 
-		$.getJSON(bvhost+"data/batch.json?apiversion=5.4&passkey="+apikey+"&"+featuredReviewString+"&include=Products&callback=?", 
-			{dataType: 'json'},
-			function(json){
-				if(typeof options.productId === 'array') { // handle array of products
-					options.productId.forEach(function(element,index,array){
-						if(typeof json.BatchedResults[element].Includes.Products === 'object') {
-							reviewContent[element+'_author'] = "<div class='bvInlineReviewAuthor'><img class='bvInlineReviewAuthorAvatar' src='"+bvhost+"/static/"+displaycode+"/noAvatar.png'></img>"+json.BatchedResults[element].Results[0].UserNickname+"</div>";
-							reviewContent[element+'_rating'] = "<img src='"+bvhost+"/"+displaycode+"/"+json.BatchedResults[element].Results[0].Rating+"/"+json.BatchedResults[element].Results[0].RatingRange+"/rating.gif' class='bvInlineRatingImage'></img>";
-							reviewContent[element+'_text'] = "<div class='bvInlineReviewText'>"+json.BatchedResults[element].Results[0].ReviewText+"</div>";
-							reviewContent[element+'_link'] = "<a href='"+bvhost+"/"+displaycode+"/"+json.BatchedResults[element].Results[0].ProductId+"/review/"+json.BatchedResults[element].Results[0].Id+"/redirect.htm' class='bvInlineReadMore'>Read More</a>";
-							reviewContent[element+'_name'] = "<div class='bvInlineReviewProductName'>"+json.BatchedResults[element].Includes.Products[json.BatchedResults[element].Includes.ProductsOrder[0]].Name+"</div>";
-							reviewContent[element+'_location'] = "<div class='bvInlineReviewProductLocation'>"+json.BatchedResults[element].Includes.Products[json.BatchedResults[element].Includes.ProductsOrder[0]].Attributes.LocationName.Values[0].Value+"</div>";
-						}
-					});
-				}
-				else { // handle single product
-					if(typeof json.BatchedResults[options.productId].Includes.Products === 'object') {
-						reviewContent[options.productId+'_author'] = "<div class='bvInlineReviewAuthor'><img class='bvInlineReviewAuthorAvatar' src='"+bvhost+"/static/"+displaycode+"/noAvatar.png'></img>"+json.BatchedResults[options.productId].Results[0].UserNickname+"</div>";
-						reviewContent[options.productId+'_rating'] = "<img src='"+bvhost+"/"+displaycode+"/"+json.BatchedResults[options.productId].Results[0].Rating+"/"+json.BatchedResults[options.productId].Results[0].RatingRange+"/rating.gif' class='bvInlineRatingImage'></img>";
-						reviewContent[options.productId+'_text'] = "<div class='bvInlineReviewText'>"+json.BatchedResults[options.productId].Results[0].ReviewText+"</div>";
-						reviewContent[options.productId+'_link'] = "<a href='"+bvhost+"/"+displaycode+"/"+json.BatchedResults[options.productId].Results[0].ProductId+"/review/"+json.BatchedResults[options.productId].Results[0].Id+"/redirect.htm' class='bvInlineReadMore'>Read More</a>";
-						reviewContent[options.productId+'_name'] = "<div class='bvInlineReviewProductName'>"+json.BatchedResults[options.productId].Includes.Products[json.BatchedResults[options.productId].Includes.ProductsOrder[0]].Name+"</div>";
-						reviewContent[options.productId+'_location'] = "<div class='bvInlineReviewProductLocation'>"+json.BatchedResults[options.productId].Includes.Products[json.BatchedResults[options.productId].Includes.ProductsOrder[0]].Attributes.LocationName.Values[0].Value+"</div>";
+		function pullReviews(callback) {
+			$.getJSON(bvhost+"data/batch.json?apiversion=5.4&passkey="+apikey+"&"+featuredReviewString+"&include=Products&callback=?", 
+				{dataType: 'json'},
+				function(json){
+					if(typeof options.productId === 'array') { // handle array of products
+						options.productId.forEach(function(element,index,array){
+							if(typeof json.BatchedResults[element].Includes.Products === 'object') {
+								reviewContent[element+'_author'] = "<div class='bvInlineReviewAuthor'><img class='bvInlineReviewAuthorAvatar' src='"+bvhost+"/static/"+displaycode+"/noAvatar.gif'></img>"+json.BatchedResults[element].Results[0].UserNickname+"</div>";
+								reviewContent[element+'_rating'] = "<img src='"+bvhost+"/"+displaycode+"/"+json.BatchedResults[element].Results[0].Rating+"/"+json.BatchedResults[element].Results[0].RatingRange+"/rating.gif' class='bvInlineRatingImage'></img>";
+								reviewContent[element+'_text'] = "<div class='bvInlineReviewText'>"+json.BatchedResults[element].Results[0].ReviewText+"</div>";
+								reviewContent[element+'_link'] = "<a href='"+bvhost+"/"+displaycode+"/"+json.BatchedResults[element].Results[0].ProductId+"/review/"+json.BatchedResults[element].Results[0].Id+"/redirect.htm' class='bvInlineReadMore'>Read More</a>";
+								reviewContent[element+'_name'] = "<div class='bvInlineReviewProductName'>"+json.BatchedResults[element].Includes.Products[json.BatchedResults[element].Includes.ProductsOrder[0]].Name+"</div>";
+							}
+						});
 					}
+					else if(typeof json.BatchedResults[options.productId] === 'object') { // handle single product
+						if(typeof json.BatchedResults[options.productId].Includes.Products === 'object') {
+							reviewContent[options.productId+'_author'] = "<div class='bvInlineReviewAuthor'><img class='bvInlineReviewAuthorAvatar' src='"+bvhost+"/static/"+displaycode+"/noAvatar.gif'></img>"+json.BatchedResults[options.productId].Results[0].UserNickname+"</div>";
+							reviewContent[options.productId+'_rating'] = "<img src='"+bvhost+"/"+displaycode+"/"+json.BatchedResults[options.productId].Results[0].Rating+"/"+json.BatchedResults[options.productId].Results[0].RatingRange+"/rating.gif' class='bvInlineRatingImage'></img>";
+							reviewContent[options.productId+'_text'] = "<div class='bvInlineReviewText'>"+json.BatchedResults[options.productId].Results[0].ReviewText+"</div>";
+							reviewContent[options.productId+'_link'] = "<a href='"+bvhost+"/"+displaycode+"/"+json.BatchedResults[options.productId].Results[0].ProductId+"/review/"+json.BatchedResults[options.productId].Results[0].Id+"/redirect.htm' class='bvInlineReadMore'>Read More</a>";
+							reviewContent[options.productId+'_name'] = "<div class='bvInlineReviewProductName'>"+json.BatchedResults[options.productId].Includes.Products[json.BatchedResults[options.productId].Includes.ProductsOrder[0]].Name+"</div>";
+						}
+					}
+					else {
+						console.log('Error: Node structure mismatch');
+					}
+					callback();
 				}
-				options.callback();
-			}
-		);
-	}
+			);
+		}
+	};
 
-})(jQuery);
+}(jQuery));
