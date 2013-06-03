2dda8e7bf72eb99bf966d927b7e48c90ad1b9e61
bdb-bv <ben.balentine@bazaarvoice.com>
2013-06-03 02:24:19 -0500

    refactored code to be more readable and partially support C13

--- js/bvSnippets.js
+++ js/bvSnippets.js
@@ -1,9 +1,9 @@
 (function($){
 /*******************************************************************************************/    
-// jquery.bvSnippets.js - version 0.1
+// jquery.bvSnippets.js - version 1.0
 // A jQuery plugin for injecting common Bazaarvoice Data API snippets
 // 
-// Copyright (c) 2013, Liam Wiles, Ben Balentine
+// Copyright (c) 2013, Ben Balentine
 // Bazaarvoice PRREP Project
 // https://github.com/bazaarvoice/BVSnippets
 // Created: 2013-05-10
@@ -12,12 +12,11 @@
 // for displaying a featured review
 
 // The following elements are requried:
-// - bvhost
-// - displaycode
 // - apikey
 
 // If you omit any optional elements, be sure to exclude the element
 // completely instead of providing an empty element.
+// Note that PRR clients must specify a legacy_hostname and legacy_displaycode in order for deep links to render correctly
 
 // Copyright 2013 Bazaarvoice, Inc.
 
@@ -27,8 +26,7 @@
 // uninterrupted use, merchantability, or fitness for a particular purpose.
 /*******************************************************************************************/
 
-
-	$.fn.featuredReviews = function(bvhost, displaycode, apikey, options){
+	$.fn.featuredReviews = function(apikey, options){
 
 		//define option defaults.  this also enumerates all possible options
 		options = {
@@ -36,68 +34,67 @@
 			filters: (options.filters !== undefined ? options.filters : 'IsFeatured:true'),
 			staging: (options.staging !== undefined ? options.staging : false),
 			limit: (options.limit !== undefined ? options.limit : '1'),
-			apiversion: (options.apiversion !== undefined ? options.apiversion : '5.4')
+			apiversion: (options.apiversion !== undefined ? options.apiversion : '5.4'),
+			legacy_hostname: (options.legacy_hostname !== undefined ? options.legacy_hostname : false ), //false indicates C13 client
+			legacy_displaycode: (options.displaycode !== undefined ? options.displaycode : false ), //false indicates C13 client
+			content_path: (options.legacy_hostname && options.legacy_displaycode ? options.legacy_hostname+( !options.staging ? '' : '/bvstaging' )+'/'+options.legacy_displaycode+'/' : (options.staging ? 'http://display.ugc.bazaarvoice.com' : 'http://display-stg.ugc.bazaarvoice.com'))
 		};
-
-		var myThis = this;
 		
-		var productId = [];
+		/* GLOBALS FOR REVIEWS */ 
 		var reviewContent = {}; //used to capture inline rating object callback
 		var featuredReviewString = ''; //used to concatenate products for batch query
 		var featuredReviewList = {}; //list of products to query
+		var queryString = '';
 
-		var template = 
-		'\<div class\=\"BVFRWContainer\"\>\<div class\=\"BVFRWProductImage\"\>\<img src\=\"{{SubjectImage}}\"\>\</div\>\<div class\=\"BVFRWContainerHeader\"\>\<div class\=\"BVFRWReviewTitle\"\>\<a href\=\"{{ReviewDeepLink}}\"\>{{ReviewTitle}}\</a\>\</div\>\<div class\=\"BVFRWInlineReviewAuthor\"\>\<span class\=\"BVFRWReviewBy\"\>By:\</span\>{{ReviewAuthor}}\</div\>\</div\>\<div class\=\"BVFRWContent\"\>\<div class\=\"BVFWRatingWrapper\"\>\<div class\=\"BVFWRatingBackground\"\>{{ReviewRatingRange}}\</div\>\<div class\=\"BVFRWRating\"\>{{ReviewRating}}\</div\>\</div\>\<div class\=\"BVFRWproductName\"\>{{SubjectName}}\</div\>\<div class\=\"BVFRWReviewText\"\>{{ReviewText}}\</div\>\<a class\=\"BVFRWReadMore\" href\=\"{{ReviewDeepLink}}\"\>Read More\</a\>\</div\>\</div\>';
+		var reviewsTemplate = {
+			DOM: '\<div class\=\"BVFRWContainer\"\>\<div class\=\"BVFRWProductImage\"\>\<img src\=\"{{SubjectImage}}\"\>\</div\>\<div class\=\"BVFRWContainerHeader\"\>\<div class\=\"BVFRWReviewTitle\"\>\<a href\=\"{{ReviewDeepLink}}\"\>{{ReviewTitle}}\</a\>\</div\>\<div class\=\"BVFRWInlineReviewAuthor\"\>\<span class\=\"BVFRWReviewBy\"\>By:\</span\>{{ReviewAuthor}}\</div\>\</div\>\<div class\=\"BVFRWContent\"\>\<div class\=\"BVFWRatingWrapper\"\>\<div class\=\"BVFWRatingBackground\"\>{{ReviewRatingRange}}\</div\>\<div class\=\"BVFRWRating\"\>{{ReviewRating}}\</div\>\</div\>\<div class\=\"BVFRWproductName\"\>{{SubjectName}}\</div\>\<div class\=\"BVFRWReviewText\"\>{{ReviewText}}\</div\>\<a class\=\"BVFRWReadMore\" href\=\"{{ReviewDeepLink}}\"\>Read More\</a\>\</div\>\</div\>',
+			Tokens: { //key should be the token, and assigned to path in the json result relative to the individual review
+				SubjectImage: 'productNode.ImageUrl',
+				ReviewDeepLink: ( !options.legacy_hostname && !options.legacy_displaycode ? 'productNode.ProductPageUrl+"#review/"+reviewElement.Id' : '"'+options.content_path+'"+reviewElement.ProductId+"/review/"+reviewElement.Id+"/redirect.htm"'), //this one is weird, but has to be here and escaped.
+				ReviewTitle: 'reviewElement.Title',
+				ReviewAuthor: 'reviewElement.UserNickname',
+				ReviewRatingRange: 'renderStars(reviewElement.RatingRange)',
+				ReviewRating: 'renderStars(reviewElement.Rating)',
+				SubjectName: 'productNode.Name',
+				ReviewText: 'reviewElement.ReviewText',
+			}
+		}; //To remove or hide elements from this template, use CSS rather than editing the DOM
 
-		this.each(function(element, index, array){
+		$.each(this, function(element, index, array){ //this builds the collection that associates each DOM with its productId and sets up the query string for each product
 			var currentProduct = $(this).attr("data-id");
-			productId.push(currentProduct);
-			featuredReviewString+='&resource.'+currentProduct+'=reviews&filter.'+currentProduct+'='+options.filters+'&Limit.'+currentProduct+'='+options.limit+'&Filter.'+currentProduct+'=productId:'+currentProduct+'&Sort.'+currentProduct+'='+options.sort;
-
-			// featuredReviewString += 'resource=reviews&Filter='+options.filters+'&Filter.'+currentProduct+'=productId:'+currentProduct+'&Sort='+options.sort+'&Limit='+options.limit;
-			featuredReviewList[currentProduct] = this;
+			featuredReviewString += '&resource.'+currentProduct+'=reviews&Filter.'+currentProduct+'=productId:'+currentProduct;
+			featuredReviewList[element] = {Node: this, productId: currentProduct};
 		});
 
-		$.getJSON((options.staging !== undefined && !options.staging ? 'http://api.bazaarvoice.com' :'http://stg.api.bazaarvoice.com')+"/data/batch.json?apiversion="+options.apiversion+"&passkey="+apikey+"&"+featuredReviewString+"&include=Products&callback=?", 
-			{dataType: 'json'},
-			function(json){
-				productId.forEach(function(element,index,array){
-					if(typeof json.BatchedResults[element].Includes.Products === 'object') {
-						var productNode = json.BatchedResults[element].Includes.Products[json.BatchedResults[element].Includes.ProductsOrder[0]]; //Product Information
-						var reviewsNode = json.BatchedResults[element].Results; //All Review Content
-						reviewContent[element] = '';
-						reviewsNode.forEach(function(reviewElement,reviewIndex,reviewArray){
-							reviewContent[element] += template
-								.replace(/{{SubjectImage}}/g,  productNode.ImageUrl)
-								.replace(/{{ReviewDeepLink}}/g,  bvhost+( !options.staging ? '' : '/bvstaging' )+'/'+displaycode+'/'+reviewElement.ProductId+'/review/'+reviewElement.Id+'/redirect.htm')
-								.replace(/{{ReviewRating}}/g,  renderStars(reviewElement.Rating))
-								.replace(/{{ReviewRatingRange}}/g,  renderStars(reviewElement.RatingRange))
-								.replace(/{{ReviewTitle}}/g,  reviewElement.Title)
-								.replace(/{{ReviewAuthor}}/g,  reviewElement.UserNickname)
-								.replace(/{{SubjectName}}/g,  productNode.Name)
-								.replace(/{{ReviewText}}/g,  reviewElement.ReviewText);
-						});
-					}
-				});
+		queryString = (options.staging !== undefined && !options.staging ? 'http://api.bazaarvoice.com' :'http://stg.api.bazaarvoice.com')+"/data/batch.json?apiversion="+options.apiversion+"&passkey="+apikey+"&"+featuredReviewString+"&filter="+options.filters+"&include=Products&Limit="+options.limit+"&Sort="+options.sort+"&callback=?";
 
-				$.each(featuredReviewList, function(index, value){
-					if(typeof reviewContent[index] === 'string') {
-						$(value).replaceWith(reviewContent[index]);
-					}
-					else {
-						console.log('Error:' + reviewContent);
-						console.log('Error:' + index);
-						console.log('Error:' + value);
-					}
-				});
+		$.getJSON(queryString, {dataType: 'json'},
+			function(json){
+				renderResults(json, featuredReviewList, reviewsTemplate);
 			}
 		);
-		// console.log(myThis); //debugging log for this object
 	};
 
+	function renderResults(resultsJson, resultsList, domTemplate) { //TODO: remove review specific data
+		$.each(resultsList, function(key, value){
+			if(typeof resultsJson.BatchedResults[value.productId].Includes.Products === 'object') {
+				var productNode = resultsJson.BatchedResults[value.productId].Includes.Products[resultsJson.BatchedResults[value.productId].Includes.ProductsOrder[0]]; //Product Information
+				var reviewsNode = resultsJson.BatchedResults[value.productId].Results; //All Review Content
+				var reviewsDOM = ''; //needed to avoid an 'undefined' string appearing in the dom
+				reviewsNode.forEach(function(reviewElement,reviewIndex,reviewArray){
+					reviewsDOM += domTemplate.DOM;
+					$.each(domTemplate.Tokens, function(key, value){
+						reviewsDOM = reviewsDOM.replace(eval('/{{'+key+'}}/g'),  eval(value)); //searches template for key and replaces it with specified path
+					});
+				});
+				$(value.Node).html(reviewsDOM); //push list of reviews to the dom
+			}
+		});
+	}
+
 	function renderStars( num ) {
-			var star = '&#9733';
-			return Array(num + 1).join(star);
-		}
+		var star = '&#9733'; //note: star color and background color must be set individually in CSS
+		return Array(num + 1).join(star);
+	}
 
 }(jQuery));
